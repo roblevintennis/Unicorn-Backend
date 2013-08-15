d45080081de74b1fcfd520c28c8775fe9f8a7b48
Rob Levin <rlevin@sugarcrm.com>
2013-08-14 20:30:48 -0700

    Trying to add compass sass and heroku build pack stuff.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        compass: {             
+            dist: {           
+                options: {   
+                    sassDir: 'scss',
+                    cssDir: 'css',
+                    force: true,
+                    outputStyle: 'compressed',
+                    environment: 'production'
+                }
+            },
+            dev: {              
+                options: {
+                    sassDir: 'scss',
+                    cssDir: 'css',
+                    force: true
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.registerTask('heroku', 'compass');
+};
+
--- web.js
+++ web.js
@@ -1,13 +1,24 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 app.use(express.logger());
-
-app.get("/", function(request, response) {
-    response.send("Hi Rob");
+app.enable('jsonp callback');
+app.get('/build', function(request, response) {
+    console.log(request.query);
+    if (!request.query.primary_color || !request.query.secondary_color) {
+        res.status(400).send('Server no comprende...probably missing required http param');
+    }
+    // Here we'd:
+    // 1. parse out the query params
+    // 2. build an _options partial ... probably use a template and fill in the dynamic stuff
+    // 3. call compass compile as system call maybe
+    // 4. read the generated buttons.css in to a string
+    // 5. stuff that in css: <the_string>
+    var primaryColor = request.query.primary_color;
+    response.jsonp({css: '.foo { color: '+primaryColor+'; }'});
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+    console.log('Listening on '+ port);
 });
 
