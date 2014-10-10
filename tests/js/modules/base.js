f6b84de42ab64a1158d828aa9dbf78e2ec596c1f
Rob Levin <pair+rob@mavenlink.com>
2014-10-09 18:28:47 -0700

    Refactored test page

---
+++ tests/js/modules/base.js
@@ -0,0 +1,33 @@
+/**
+* BASE JSONP MODEL
+*/
+
+var JsonpModel = Backbone.Model.extend({
+
+  ///// REQUIRED PROPERTIES ////////
+  module: null,
+  defaults: null,
+  url: function() {},
+
+  ///// ABSTRACT METHODS /////
+  sync: function(method, model, options) {
+    var url = model.url()+"?callback=?";
+    var data = this.toJSON();
+    console.log("URL: ", url);
+    var params = _.extend({
+      type: 'GET',
+      dataType: 'jsonp',
+      data: data,
+      url: url
+    }, options);
+    return $.ajax(params);
+  },
+  parse: function(response) {
+    if (response && response[this.module]) {
+      var responseObject = {};
+      responseObject[this.module] = response[this.module];
+      responseObject.optionsScss = response.optionsScss;
+      return responseObject;
+    }
+  }
+});
---
+++ tests/js/modules/buttons.js
@@ -0,0 +1,88 @@
+/**
+* Buttons Module
+*/
+
+var ButtonsModule = ButtonsModule || {};
+
+ButtonsModule.buttonDefaultOptions = {
+  'btn-namespace': '.robs-buttons',
+  'btn-colors': [
+    {
+        name: 'primary',
+        background: '#00A1CB',
+        color: '#FFFFFF'
+    },
+    {
+        name: 'action',
+        background: '#7db500',
+        color: '#FFFFFF'
+    },
+    {
+        name: 'highlight',
+        background: '#F18D05',
+        color: '#FFFFFF'
+    },
+    {
+        name: 'caution',
+        background: '#E54028',
+        color: '#FFFFFF'
+    },
+    {
+        name: 'silly',
+        background: '#87318C',
+        color: '#FFFFFF'
+    }
+  ],
+  'btn-shapes': [
+    {
+      name: 'pill',
+      radius: '200px'
+    },
+    {
+      name: 'rounded',
+      radius: '3px'
+    }
+  ],
+  'btn-sizes': [
+    {
+      name: 'giant',
+      multiplier: '2'
+    },
+    {
+      name: 'jumbo',
+      multiplier: '1.5'
+    },
+    {
+      name: 'large',
+      multiplier: '1.25'
+    },
+    {
+      name: 'normal',
+      multiplier: '1'
+    },
+    {
+      name: 'small',
+      multiplier: '.75'
+    },
+    {
+      name: 'tiny',
+      multiplier: '.5'
+    }
+  ],
+  'types': ['border', 'shapes'],
+  'btn-height': '40px',
+  'btn-bgcolor': '#EEE',
+  'btn-font-color': '#666',
+  'btn-font-size': '14px',
+  'btn-font-weight': '300',
+  'btn-font-family': ['Arial', 'sans-serif']
+};
+
+ButtonsModule.Model = JsonpModel.extend({
+  module: 'buttons',
+  defaults: ButtonsModule.buttonDefaultOptions,
+  url: function() {
+    // return 'http://options-compiler.herokuapp.com/build/buttons';
+    return 'http://localhost:5000/build/'+this.module;
+  }
+});
