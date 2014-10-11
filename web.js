e3f937826aec1652dc147a49f8242a20cee03522
Rob Levin <pair+rob@mavenlink.com>
2014-10-11 06:49:27 -0700

    more cleanup

--- web.js
+++ web.js
@@ -1,37 +1,35 @@
 /*jshint multistr: true */
 var express = require('express'),
-    path = require('path'),
-    fs = require('fs-extra'),
-    _ = require('underscore'),
-    exec = require('child_process').exec,
-    spawn = require('child_process').spawn,
-    child,
-    app = express(),
-    styleguide = require('./lib/styleguide').styleguide,
-    compassCompileMiddleware = require('./lib/compiler').compassCompileMiddleware,
-    createOptionsMiddleware = require('./lib/options').createOptionsMiddleware,
-    createSandbox = require('./lib/safe.js').createSandbox;
+  path = require('path'),
+  fs = require('fs-extra'),
+  _ = require('underscore'),
+  exec = require('child_process').exec,
+  spawn = require('child_process').spawn,
+  child,
+  app = express(),
+  compileMiddleware = require('./lib/compiler').compileMiddleware,
+  optionsMiddleware = require('./lib/options').optionsMiddleware,
+  sandboxMiddleware = require('./lib/safe.js').sandboxMiddleware;
 
 app.configure(function() {
-    'use strict';
-    app.use(express.logger());
-    app.use(express.compress());
-    app.use(express.bodyParser());
-
+  'use strict';
+  app.use(express.logger());
+  app.use(express.compress());
+  app.use(express.bodyParser());
 });
+
 // app.configure('development', function(){})
 // app.configure('production', function(){})
 app.enable('jsonp callback');
 
-
 // Custom middleware - ensure that the _options.scss partial is written out
 // BEFORE the compass middleware does compilation.
 var customMiddleware = [
-        //Ordering here is top to bottom
-        createSandbox,//copy module dir over since we mutate ... safety first :)
-        createOptionsMiddleware,
-        compassCompileMiddleware
-    ];
+  //Ordering here is top to bottom
+  sandboxMiddleware, //copy module dir over since we mutate ... safety first :)
+  optionsMiddleware,
+  compileMiddleware
+];
 
 //////////////////////////////////////////////////////////
 ///////////////////////// ROUTES /////////////////////////
@@ -44,8 +42,8 @@ app.get('/download/:module', customMiddleware, downloadModule);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    'use strict';
-    console.log('Listening on '+ port);
+  'use strict';
+  console.log('Listening on ' + port);
 });
 
 
@@ -53,13 +51,17 @@ app.listen(port, function() {
 //////////////////// ERROR MIDDLEWARE ////////////////////
 //////////////////////////////////////////////////////////
 function logErrors(err, req, res, next) {
-    console.error(err.stack);
-    next(err);
+  console.error(err.stack);
+  next(err);
 }
+
 function errorHandler(err, req, res, next) {
     //We purposely use status 200 since using jsonp and let client handle
-    res.jsonp(200, {error: true, message: err.message || 'Server Error'});
-}
-// Add middleware (note this must be added > routes)
+    res.jsonp(200, {
+      error: true,
+      message: err.message || 'Server Error'
+    });
+  }
+  // Add middleware (note this must be added > routes)
 app.use(logErrors);
 app.use(errorHandler);
